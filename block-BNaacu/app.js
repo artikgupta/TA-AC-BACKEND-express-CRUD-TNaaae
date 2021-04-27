83145467c676b9ca1fe3a6b95d699509e59f67e7
artikgupta <artikgupta>
2021-04-27 13:44:49 +0530

    completed block-1 of Views & Routing in Express

---
+++ block-BNaacu/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var path = require('path');
+
+var studentRoutes = require('./routes/student');
+
+mongoose.connect(
+  'mongodb://localhost/sample',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log(err ? err : 'Connected to database');
+  }
+);
+var app = express();
+
+// ejs
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use('/', require('./routes/index'));
+app.use('/students', studentRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('Page not found');
+});
+app.listen(5001, () => {
+  console.log('server is running at 5001');
+});

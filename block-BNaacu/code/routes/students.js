4ed531d977dd71994851d92ba6a6bad1a3882cc3
artikgupta <artikgupta>
2021-04-27 15:13:09 +0530

    refactored

--- block-BNaacu/code/routes/students.js
+++ block-BNaacu/code/routes/students.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/new', (req, res) => {
-  res.render('form');
+   res.render('forms')
 });
 router.post('/', (req, res) => {
   res.json(req.body);
